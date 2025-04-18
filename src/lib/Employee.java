80e389825bfd716d2e0ee52d853a71d755df1acf
AhmadFadliAkbar <135632054+AhmadFadliAkbar@users.noreply.github.com>
2025-04-18 18:36:45 +0700

    Update Employee.java

--- src/lib/Employee.java
+++ src/lib/Employee.java
@@ -20,7 +20,7 @@ public class Employee {
     private int monthWorkingInYear;
     
     private boolean isForeigner;
-    private boolean gender;
+    private boolean gender; // true = Laki-laki, false = Perempuan
     
     private int monthlySalary;
     private int otherMonthlyIncome;
