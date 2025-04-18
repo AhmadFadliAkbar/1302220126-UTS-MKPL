4910d3e7e04da098590fe09bdf3f1ca455fefd65
AhmadFadliAkbar <135632054+AhmadFadliAkbar@users.noreply.github.com>
2025-04-18 18:53:37 +0700

    Gabung monthlySalary & otherMonthlyIncome ke monthlyIncome

--- src/lib/TaxFunction.java
+++ src/lib/TaxFunction.java
@@ -1,8 +1,8 @@
 package lib;
 
 public class TaxFunction {
+    
 
-	
 	/**
 	 * Fungsi untuk menghitung jumlah pajak penghasilan pegawai yang harus dibayarkan setahun.
 	 * 
@@ -14,31 +14,29 @@ public class TaxFunction {
 	 * 
 	 */
 	
-	
-	public static int calculateTax(int monthlySalary, int otherMonthlyIncome, int numberOfMonthWorking, int deductible, boolean isMarried, int numberOfChildren) {
-		
-		int tax = 0;
-		
-		if (numberOfMonthWorking > 12) {
-			System.err.println("More than 12 month working per year");
-		}
-		
-		if (numberOfChildren > 3) {
-			numberOfChildren = 3;
-		}
-		
-		if (isMarried) {
-			tax = (int) Math.round(0.05 * (((monthlySalary + otherMonthlyIncome) * numberOfMonthWorking) - deductible - (54000000 + 4500000 + (numberOfChildren * 1500000))));
-		}else {
-			tax = (int) Math.round(0.05 * (((monthlySalary + otherMonthlyIncome) * numberOfMonthWorking) - deductible - 54000000));
-		}
-		
-		if (tax < 0) {
-			return 0;
-		}else {
-			return tax;
-		}
-			 
-	}
-	
+
+    public static int calculateTax(int monthlyIncome, int numberOfMonthWorking, int deductible, boolean isMarried, int numberOfChildren) {
+        
+        int tax = 0;
+        
+        if (numberOfMonthWorking > 12) {
+            System.err.println("More than 12 month working per year");
+        }
+        
+        if (numberOfChildren > 3) {
+            numberOfChildren = 3;
+        }
+        
+        if (isMarried) {
+            tax = (int) Math.round(0.05 * ((monthlyIncome * numberOfMonthWorking) - deductible - (54000000 + 4500000 + (numberOfChildren * 1500000))));
+        } else {
+            tax = (int) Math.round(0.05 * ((monthlyIncome * numberOfMonthWorking) - deductible - 54000000));
+        }
+        
+        if (tax < 0) {
+            return 0;
+        } else {
+            return tax;
+        }
+    }
 }
